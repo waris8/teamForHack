43417d429f4fa8a3ec359c2411f20bb668ba22ee
waris8 <gwaris98@gmail.com>
2024-04-17 01:17:30 +0530

    initial commit

---
+++ src/main/java/com/teamhack/teamForHack/TeamForHackApplication.java
@@ -0,0 +1,13 @@
+package com.teamhack.teamForHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamForHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamForHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamhack/teamForHack/TeamForHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhack.teamForHack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamForHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
