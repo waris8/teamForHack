aa6c0369130ce737d9a00a2290a0cbc1a7cc4833
waris8 <gwaris98@gmail.com>
2024-04-18 00:09:37 +0530

    added user entity

---
+++ src/main/java/com/teamhack/teamForHack/entity/SocialMediaLinks.java
@@ -0,0 +1,8 @@
+package com.teamhack.teamForHack.entity;
+
+public class SocialMediaLinks {
+
+    private String linkedIn;
+    private String github;
+    private String portfolio;
+}
---
+++ src/main/java/com/teamhack/teamForHack/entity/User.java
@@ -0,0 +1,12 @@
+package com.teamhack.teamForHack.entity;
+
+import java.io.Serializable;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String email;
+    private long phone;
+    private SocialMediaLinks socialMediaLinks;
+}
